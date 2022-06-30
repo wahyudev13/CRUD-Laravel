cfbbc09ca47886c1b8043c9dc8602bb433c8485f
Mohammad Feryzal Fahlevi <mferyzalfahlevi29@gmail.com>
2022-06-30 13:06:37 +0700

    commit ketiga

--- resources/views/report/kader.blade.php
+++ resources/views/report/kader.blade.php
@@ -70,12 +70,8 @@
                 </tbody>
               </table>
               <!-- End Table with stripped rows -->
-          
-              
-
             </div>
           </div>
-
         </div>
       </div>
     </section>
